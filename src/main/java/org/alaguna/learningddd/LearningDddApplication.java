752f60a57a307461c3196ceb1b6eb25e7adbc9b9
alaguna95 <albertolh3@gmail.com>
2021-03-29 18:12:29 +0200

    first commit

---
+++ src/main/java/org/alaguna/learningddd/LearningDddApplication.java
@@ -0,0 +1,13 @@
+package org.alaguna.learningddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningDddApplication.class, args);
+	}
+
+}
